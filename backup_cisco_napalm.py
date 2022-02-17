fb368c6db411000eaafc82a7872f6542a13d5056
Gabriel Guerra <gabriel.guerra25@outlook.com>
2022-02-17 23:21:50 +0000

    added utils functions

--- backup_cisco_napalm.py
+++ backup_cisco_napalm.py
@@ -1,42 +1,32 @@
 from napalm import get_network_driver
 import datetime, os
+from utils import createPathBackup, createNameBackup
 
 #device_list = ['172.16.1.2','172.16.1.254']
 #user = 'giguerra'
 #passwd = 'cisco'
 
-def backup(ip,user,passwd):
+def backupNapalm(ip,user,passwd):
     
-    path = os.getcwd()
-
-    try:
-        os.stat(path+'/backup_config')
-    except:
-        os.mkdir(path+'/backup_config')
-
-    #device_list = ['172.16.1.2','172.16.1.254']
-
-    #user = 'giguerra'
-    #passwd = 'cisco'
+    path = createPathBackup()
 
     driver = get_network_driver('ios')
-    #pp = pprint.PrettyPrinter(indent=4)
 
     with driver(hostname=ip, username=user, password=passwd) as device:
 
         config = device.get_config(retrieve='running')
         run_conf = config['running']
         
-    date = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    filename =  createNameBackup(ip)
 
-    with open(path+'/backup_config/'+ip+'_'+date+'_'+'running-config','w') as file:
+    with open(path+filename,'w') as file:
 
         file.write(run_conf)
-            #pp.pprint(run_conf)
 
-def restore(file,ip,user,passwd):
+
+def restoreNapalm(file,ip,user,passwd):
     
-    path = os.getcwd()+'/backup_config'
+    path = createPathBackup()
 
     driver = get_network_driver('ios')
 
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from backup_cisco_napalm import backup, restore
+from backup_cisco_napalm import backupNapalm, restoreNapalm
+from backup_cisco_netconf import backupNetconf, restoreNetconf
 import os
 
 device_list = ['172.16.1.2','172.16.1.254']
 user = 'giguerra'
 passwd = 'cisco'
-
+dir = os.getcwd()+'/backup_config/'
 bef = 'nochange_config'
 aft = 'change_config'
-#backup(device_list[1],user,passwd)
 
-restore(os.getcwd()+'/backup_config/'+'R3',device_list[1],user,passwd)
+#Napalm
+#backupNapalm(device_list[0],user,passwd)
+#restoreNapalm(dir+'nochange_config_S1',device_list[0],user,passwd)
+
+#NETCONF
+#backupNetconf(device_list[1],user,passwd)
+#restoreNetconf(dir+'change_config_R3.xml',device_list[1],user,passwd)
