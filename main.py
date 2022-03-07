8d396e6ea8e6d72a3b0cbef31c4fb6c3392aa030
Gabriel Guerra <gabriel.guerra25@outlook.com>
2022-03-07 16:40:35 +0000

    added backup&restore for rest api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from backup_cisco_napalm import backupNapalm, restoreNapalm
 from backup_cisco_netconf import backupNetconf, restoreNetconf
+from backup_mikrotik_rest import backupRestApi, restoreRestApi
 import os
 
-device_list = ['172.16.1.2','172.16.1.254']
+device_list = ['172.16.1.2','172.16.1.254','10.0.0.2']
 user = 'giguerra'
 passwd = 'cisco'
 dir = os.getcwd()+'/backup_config/'
@@ -16,3 +17,7 @@ aft = 'change_config'
 #NETCONF
 #backupNetconf(device_list[1],user,passwd)
 #restoreNetconf(dir+'change_config_R3.xml',device_list[1],user,passwd)
+
+#RESTAPI
+#backupRestApi(device_list[2],user,passwd)
+#restoreRestApi(dir+'change_config_R2.backup',device_list[2],user,passwd)
