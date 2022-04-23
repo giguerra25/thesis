3754b9281d548245aac3cb25935e496a6f6f05ba
Gabriel Guerra <gabriel.guerra25@outlook.com>
2022-04-23 21:51:13 +0000

    added configuration vlans

--- config_cisco_napalm.py
+++ config_cisco_napalm.py
@@ -1,4 +1,5 @@
 from napalm import get_network_driver
+from netmiko import ConnectHandler
 from napalm.base.exceptions import ConnectionException
 import datetime
 from jinja2 import Template
@@ -78,7 +79,7 @@ class ConfigInterface(Config):
         self.request("tmp/conf.txt")
 
         os.remove("tmp/conf.txt")"""
-        
+        print(configuration_data)
         self.request(configuration_data)
 
 
@@ -95,17 +96,46 @@ class ConfigStaticRoute(Config):
 
         configuration_data = napalm_template.render(route_list=self.list_routes)
         print(configuration_data)
-        """output_file = "tmp/conf.txt"
-        with open(output_file, "w") as f:
-            f.write(configuration_data)
-        
-        self.request("tmp/conf.txt")
-
-        os.remove("tmp/conf.txt")"""
 
         self.request(configuration_data)
 
 
+class ConfigVlan(Config):
+
+    def __init__(self, ip, user, passwd, list_vlans=[]):
+        super().__init__(ip, user, passwd)
+
+        self.list_vlan = list_vlans
+
+
+    def netmikoconfig(self,command):
+
+        device = {"device_type":'cisco_ios',
+                  "ip":self.ip,
+                  "username":self.user,
+                  "global_delay_factor": 2,
+                  "password":self.passwd}
+
+        net = ConnectHandler(**device)
+        output = net.send_config_set(command)
+        return output
+
+    def config_vlans(self):
+
+        napalm_template = Template(open("templates/napalm_template/config_vlan.j2").read())
+
+        configuration_data = napalm_template.render(vlan_list=self.list_vlan)
+        #print(configuration_data)
+
+        #VLAN creation using Netmiko
+        conf_vlan = []
+        for vlan in self.list_vlan:
+            conf_vlan.append("vlan {}".format(vlan["id"]))
+            conf_vlan.append("name {}".format(vlan["name"]))
+        #print(conf_vlan)
+
+        self.netmikoconfig(conf_vlan)
+        self.request(configuration_data)
 
 
 param = [
@@ -118,8 +148,8 @@ param = [
 ]
 
 
-a = ConfigInterface('172.16.1.2','giguerra','cisco',param)
-a.config_if()
+#a = ConfigInterface('172.16.1.2','giguerra','cisco',param)
+#a.config_if()
 
 routes =[
     {
@@ -132,3 +162,37 @@ routes =[
 
 #a = ConfigStaticRoute('172.16.1.2','giguerra','cisco',routes)
 #a.config_routes()
+
+
+vlans =[
+    {
+        "name": "vlan-150",
+        "id": "150",
+        "interfaces": [
+            "GigabitEthernet3/2",
+            "GigabitEthernet3/3",
+        ],
+    },
+    {
+        "name": "vlan-120",
+        "id": "120",
+        "interfaces": [
+            "GigabitEthernet3/0",
+            "GigabitEthernet3/1",
+        ],
+    }
+]
+
+commands = ['vlan 120',
+            'name vlan-120',
+            'interface GigabitEthernet3/0',
+            'switchport',
+            'switchport mode access',
+            'switchport access vlan 120',
+   
+    
+]
+
+
+a = ConfigVlan('172.16.1.2','giguerra','cisco',vlans)
+a.config_vlans()
